badc1dda4a0888040fd417008b54afd7f985e4f5
zackad <zackad.dev@gmail.com>
2019-03-01 14:46:00 +0700

    [script] initial installer for macos system

---
+++ .bin/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#
+# This is the installer for macOS system that will install
+# dotfiles repository into home directory and enable versioning
+# using git and add an alias dot to track the change.
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install git
+# Clone Repository with "macos-branch"
+# Install php related tools
+# Install nodejs related tools
