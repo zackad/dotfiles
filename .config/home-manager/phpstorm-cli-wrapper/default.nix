6870150693aad3cee81dffe9e0c5081037cc7cfd
zackad <zackad.dev@gmail.com>
2024-02-15 07:41:06 +0700

    hm: create wrapper for phpstorm launcher from cli
    Existing launcher prevent us from using terminal. This wrapper will put
    phpstorm into background and disown it. We can safely close terminal
    that lauch it.

---
+++ .config/home-manager/phpstorm-cli-wrapper/default.nix
@@ -0,0 +1,23 @@
+{ stdenvNoCC, lib, writeShellScript }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "phpstorm-cli-wrapper";
+  version = "0.1.0";
+  src = ./.;
+
+  # Use writeScript to create the script
+  script = writeShellScript "pstorm" ''
+    phpstorm $1 > /dev/null 2>&1 & disown
+  '';
+
+  # The script is already executable, so we don't need to set permissions
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${script} $out/bin/pstorm
+  '';
+
+  meta = with lib; {
+    description = "Wrapper launcher for phpstorm from cli";
+    license = licenses.mit;
+  };
+}
