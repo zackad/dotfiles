23b06123b7fbb73b0a7786f01f4b3188ba486e6b
zackad <zackad.dev@gmail.com>
2024-05-25 21:18:18 +0700

    hm: setup as flakes

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My Home Manager Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    homeConfigurations = {
+      "zackad" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        modules = [ ./.config/home-manager/home.nix ];
+      };
+    };
+  };
+}
