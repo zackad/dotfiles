cdad0710e6bdc2cb819a9ec561c9e3ed218386cc
zackad <zackad.dev@gmail.com>
2024-05-27 21:06:47 +0700

    hm: setup home-manager as flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Home Manager for MacOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+
+    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+
+    homeConfigurations = {
+      "zackad" = home-manager.lib.homeManagerConfiguration {
+        # Note: I am sure this could be done better with flake-utils or something
+        pkgs = import nixpkgs { system = "x86_64-darwin"; };
+
+        modules = [ ./.config/home-manager/home.nix ]; # Defined later
+      };
+    };
+
+  };
+}
+
+
