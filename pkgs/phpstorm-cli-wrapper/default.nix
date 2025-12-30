c3f097f71eb17caad3880997ed0571647bdc42c0
zackad <zackad@zackad.dev>
2025-12-30 13:14:53 +0700

    fix: different phpstorm cli launcher for different linux distro

--- pkgs/phpstorm-cli-wrapper/default.nix
+++ pkgs/phpstorm-cli-wrapper/default.nix
@@ -2,6 +2,7 @@
   stdenvNoCC,
   lib,
   writeShellScript,
+  config,
 }:
 
 stdenvNoCC.mkDerivation rec {
@@ -10,9 +11,17 @@ stdenvNoCC.mkDerivation rec {
   src = ./.;
 
   # Use writeScript to create the script
-  script = writeShellScript "pstorm" ''
-    steam-run $HOME/Applications/PhpStorm/bin/phpstorm "$@" > /dev/null 2>&1 & disown
-  '';
+  script =
+    if lib.hasAttr "boot" config then
+      # Using steam-run to run phpstorm that manually installed (not from nixpkgs)
+      writeShellScript "pstorm" ''
+        steam-run $HOME/Applications/PhpStorm/bin/phpstorm "$@" > /dev/null 2>&1 & disown
+      ''
+    else
+      # For regular linux distro, assuming you have added phpstorm cli launcher into your PATH
+      writeShellScript "pstorm" ''
+        phpstorm "$@" > /dev/null 2>&1 & disown
+      '';
 
   # The script is already executable, so we don't need to set permissions
   installPhase = ''
