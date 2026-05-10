1fe976787bff8d8147cff79186cb7af38832c4c1
zackad <zackad@zackad.dev>
2026-05-11 06:21:04 +0700

    chore: add flake input to add support for dendritic pattern

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
