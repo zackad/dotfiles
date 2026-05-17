0fbc758e79c788d159abc7c9bcd2b4676c8b805f
zackad <zackad@zackad.dev>
2026-05-17 08:55:35 +0700

    fix: install waybar and wl-clipboard from home-manager

--- modules/features/hyprland/default.nix
+++ modules/features/hyprland/default.nix
@@ -1,9 +1,16 @@
 { inputs, ... }:
 {
-  flake.homeModules.nixosModule = {
-    wayland.windowManager.hyprland = {
-      enable = true;
-      extraConfig = builtins.readFile ./hyprland.conf;
+  flake.homeModules.nixosModule =
+    { pkgs, ... }:
+    {
+      home.packages = [
+        pkgs.waybar
+        pkgs.wl-clipboard
+      ];
+
+      wayland.windowManager.hyprland = {
+        enable = true;
+        extraConfig = builtins.readFile ./hyprland.conf;
+      };
     };
-  };
 }
