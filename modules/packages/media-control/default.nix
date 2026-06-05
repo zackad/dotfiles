a14a811b793dfb5edfc485b5b7e7b1d1b22f9618
zackad <zackad@zackad.dev>
2026-06-05 08:52:03 +0700

    hm: disable workaround for Plexamp media key
    After updating home-manager and nixpkgs it seems that playerctl can
    detect Plexamp properly.

--- modules/packages/media-control/default.nix
+++ modules/packages/media-control/default.nix
@@ -8,21 +8,21 @@
         # Use writeScript to create the script
         text = ''
           # The command to send to Plexamp
-          PLEX_PORT=32500
+          # PLEX_PORT=32500
           ACTION=$1 # play-pause, next, or previous
 
           # Map playerctl actions to Plexamp API endpoints
-          case $ACTION in
-            play-pause) PLEX_CMD="playPause" ;;
-            next)       PLEX_CMD="skipNext" ;;
-            previous)   PLEX_CMD="skipPrevious" ;;
-          esac
+          # case $ACTION in
+          #   play-pause) PLEX_CMD="playPause" ;;
+          #   next)       PLEX_CMD="skipNext" ;;
+          #   previous)   PLEX_CMD="skipPrevious" ;;
+          # esac
 
           # If a player (like Firefox/Chrome) is active, use playerctl
           ${pkgs.playerctl}/bin/playerctl "$ACTION"
 
           # Otherwise, fallback to Plexamp API
-          ${pkgs.curl}/bin/curl "http://127.0.0.1:$PLEX_PORT/player/playback/$PLEX_CMD"
+          # ${pkgs.curl}/bin/curl "http://127.0.0.1:$PLEX_PORT/player/playback/$PLEX_CMD"
         '';
       };
     };
